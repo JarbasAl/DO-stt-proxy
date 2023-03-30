9e80045c6ffd475e4794cb6a6b1a5736f6879d3e
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-03-30 03:43:34 +0100

    Update __main__.py

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -1,10 +1,26 @@
-import pyjokes
+from ovos_plugin_manager.stt import load_stt_plugin
+from ovos_utils.log import LOG
+from speech_recognition import Recognizer, AudioFile, AudioData
+from ovos_stt_plugin_chromium import ChromiumSTT
+from tempfile import NamedTemporaryFile
 
+def bytes2audiodata(data):
+    recognizer = Recognizer()
+    with NamedTemporaryFile() as fp:
+        fp.write(data)
+        with AudioFile(fp.name) as source:
+            audio = recognizer.record(source)
+    return audio
+
+  
 def main(args):
-  joke = pyjokes.get_joke()
+  stt = ChromiumSTT()
+  lang = args.get("lang", "en-us").lower()
+  # TODO - check format of args to see how audio is passed
+  #audio = args.get("audio")
+  #utt = stt.execute(bytes2audiodata()audio)
   return {
     'body': {
-      'response_type': 'in_channel',
-      'text': joke
+      'args': str(args)
     }
   }
